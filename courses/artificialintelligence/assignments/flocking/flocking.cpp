f759497341b55be71e3ae817a28aafeb3bb85148
William-Box-Champlain <william.box@mymail.champlain.edu>
2023-09-21 13:28:32 -0400

    Finished xorshift project

--- courses/artificialintelligence/assignments/flocking/flocking.cpp
+++ courses/artificialintelligence/assignments/flocking/flocking.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <list>
+
+boid {
+  float mXCoordinate;
+  float mYCoordinate;
+  float mVelocityX;
+  float mVelocityY;
+};
+
+void flock(float CohesionRadius, float SeparationRadius, float SeparationForceMax,
+           float AlignmentRadius, float CohesionConstant, float SeparationConstant,
+           float AlignmentConstant, int AgentsCount);
 
 int main(){
+
+}
+
+void flock(float CohesionRadius, float SeparationRadius, float SeparationForceMax, float AlignmentRadius, float CohesionConstant, float SeparationConstant, float AlignmentConstant, int AgentsCount)
+{
 
 }
--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,30 @@
+#include <iostream>
+#include <cstdint>
 // add your imports here
+int xorShift(int input,int seed,int min,int max);
+
 int main(){
-  // code here
+    uint32_t seed, outputs, min, max;
+    std::cin >> seed >> outputs >> min >> max;
+    xorShift(seed,outputs,min,max);
+}
+
+int xorShift(int seed,int outputs,int min,int max)
+{
+    int leftOne = 13;
+    int rightOne = 17;
+    int leftTwo = 5;
+    int random = seed;
+    for(int i = 0; i < outputs; i++)
+    {
+        //generate random
+        random ^= random << leftOne;
+        random ^= random >> rightOne;
+        random ^= random << leftTwo;
+        abs(random);
+        //clamp random
+        int clampedValue = min + (random % (max - min + 1));
+        std::cout << clampedValue << std::endl;
+    }
+    return random;
 }
